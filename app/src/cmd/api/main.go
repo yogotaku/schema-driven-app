45922d31b2f499f862f12260097590220ef08be7
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2023-02-18 17:46:03 +0900

    APIサーバーの起動処理

--- app/src/cmd/api/main.go
+++ app/src/cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	oapiMiddleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/yogotaku/schema-driven-app/app/src/infrastructure"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// スキーマの定義を取得
+	swagger, _ := infrastructure.GetSwagger()
+
+	// スキーマのServerInterfaceを実装した型を取得
+	server := infrastructure.NewApiServer()
+
+	r := chi.NewRouter()
+
+	// リクエストがスキーマの定義に合っているかのバリデーション
+	r.Use(oapiMiddleware.OapiRequestValidator(swagger))
+
+	// panicが起きた際に復帰する
+	r.Use(middleware.Recoverer)
+
+	infrastructure.HandlerFromMux(server, r)
+
+	http.ListenAndServe(":80", r)
+
 }
