25bd95402787e830040a47bb47bd6a33a33a21e9
Takuya Yogo <62864518+yogotaku@users.noreply.github.com>
2023-02-19 19:01:40 +0900

    使用するドメインの型作成

---
+++ frontend/vite-app/src/type.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: number
+  firstName: string
+  lastName: string
+  email: string
+  dateOfBirth: string
+  createDate: string
+}
+
+export interface Pet {
+  id: number
+  tag: string
+  name: string
+}
